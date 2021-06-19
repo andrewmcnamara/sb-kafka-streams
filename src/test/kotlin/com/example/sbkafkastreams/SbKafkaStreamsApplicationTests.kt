40e31178cb3115a8edd26390824f2abf8236a2e0
Andrew McNamara <andrew.mcnamara@bugcrowd.com>
2021-06-19 14:22:46 +1000

    first commit

---
+++ src/test/kotlin/com/example/sbkafkastreams/SbKafkaStreamsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sbkafkastreams
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SbKafkaStreamsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
